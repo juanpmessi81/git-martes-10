8c5a5e6b00b391cc635454df6848fe8ab548bdca
Cristian Racedo <cristiandracedo@hotmail.com>
2021-08-31 11:14:35 -0300

    Estilos: Tabla COlaboradores

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,4 @@
     menuBtn = document.querySelector('nav .btn')
     menuLst = document.querySelector('nav .menu')
 
-    menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+    menuBtn.onclick = menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
